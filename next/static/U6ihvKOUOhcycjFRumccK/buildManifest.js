dba4878527d8fdf9c55690a0efb8be367ee3774f
moonlight41131 <moonlight41131@outlook.com>
2024-05-10 14:01:18 -0700

    career part fixed

--- next/static/U6ihvKOUOhcycjFRumccK/buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-cbc5880d288c1403.js"],"/_error":["static/chunks/pages/error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
