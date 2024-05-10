bb2a7bf21ad6367063dd77212c45e06768800286
moonlight41131 <moonlight41131@outlook.com>
2024-05-10 09:16:38 -0700

    project swapped

---
+++ next/static/U6ihvKOUOhcycjFRumccK/buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-cbc5880d288c1403.js"],"/_error":["static/chunks/pages/error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- next/static/uge3Qlax0sWti56x9YB_Q/buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-bb030a0067e0c538.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
