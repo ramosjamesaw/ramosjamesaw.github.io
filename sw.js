2b11532e835840567464878e37d22e8e97d9305e
codeNcool <codeNcool@outlook.com>
2024-06-19 03:55:47 -0700

    carreer changed

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-37cc91eac3708ee009b6.js"
+    "url": "webpack-runtime-8a1c8dcdd8ebbd693150.js"
   },
   {
     "url": "framework-306428b3f19363da004c.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "66f46e5c9df198a06634f1ff6cbfbb79"
+    "revision": "51cf225476ed976e875785a942db0d09"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-6f061b09cc63f0400406.js"
